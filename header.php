65e43fb2cac237d5471bc430829b6dd0b4635aac
sushil-poudel <sushilpoudel869@gmail.com>
2021-11-22 12:34:44 +0545

    adding files for the first time

---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>
+
+        </title>
+        <!-- META STARTS -->
+            <meta charset="UTF-8">
+            <meta http-eqiv="X-UA-Compatible" content="IE-edge">
+            <meta name="viewport" conent="width=device-width">
+            <link rel="shortcut icon" type="image/x-icon" href="img/site_logo.png">
+        
+
+            <meta property="og:title" content=""/>
+            <meta property="og:type"   content="website" />
+            <meta property="og:url"    content="" />
+            <meta property="og:site_name" content=""/>
+            <meta property="og:image" content=""/>
+            <meta property="og:image:alt" content="">
+            <meta property="og:description" content="">
+        <!-- META ENDS -->
+
+
+        <!-- COMPONENTS STARTS -->
+        <!-- COMPONENTS ENDS -->
+
+
+        <!-- SITE STYLE STARTS -->
+            <link href="css/global.css" rel="stylesheet">
+            <link href="css/thestyles.css" rel="stylesheet">
+            <link href="css/responsive.css" rel="stylesheet">
+        <!-- SITE STYLE ENDS -->
+    </head>
+    <body>
+        <!------------------------------------- Wrapper Starts ------------------------------------->
+    <div id="wrapper">
+
+    <!--------------------------------- Header Wrapper Starts ---------------------------------->
+    <header id="header-wrapper">
+
+    </header>
+    <!---------------------------------- Header Wrapper Ends ----------------------------------->
+
+
+
+    <!-------------------------------- Content Wrapper Starts ---------------------------------->
+    <div id="content-wrapper">
+
---
+++ js/thescripts.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Joshua Tamang
+ */
+
+
+ var winWidth = $(window).width();
+
+ $(document).ready(function () {
+     sliderInit();
+     addClassInit();
+     navInit();
+     mcustomInit();
+     matchHeight();
+ });
+ 
+ 
+ /*------------------------------- Functions Starts -------------------------------*/
+ function sliderInit() {
+     /*$('.common-banner-slider').slick({
+         arrows: false,
+         dots: true,
+         autoplay: false,
+         speed: 500,
+         fade: true,
+         pauseOnHover: false,
+         cssEase: 'linear',
+         slidesToShow: 2,
+         slidesToScroll: 1,
+         responsive: [
+             {
+                 breakpoint: 667,
+                 settings: {
+                     arrows: true,
+                     dots: false
+                 }
+             }
+         ]
+     });*/
+ 
+ }
+ 
+ function addClassInit() {
+ 
+ }
+ 
+ function navInit() {
+ 
+ }
+ 
+ function mcustomInit() {
+ 
+ }
+ 
+ 
+ 
+ /*-------------------------------- Functions Ends --------------------------------*/
+ 
